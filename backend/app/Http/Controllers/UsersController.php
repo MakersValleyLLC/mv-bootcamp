c401289d5c8330effc04c6541de24422d5e1f9f8
Denny Casabianca <casabiancadenny@gmail.com>
2018-01-03 23:08:40 +0100

    Created Controller for User CRUD actions

--- backend/app/Http/Controllers/UsersController.php
+++ backend/app/Http/Controllers/UsersController.php
@@ -6,8 +6,42 @@ use App\Users;
 
 class UsersController extends Controller
 {
-   public function __invoke()
+
+    public function __invoke()
     {
         return Users::all();
     }
+
+        public function index()
+    {
+        return Users::all();
+    }
+
+
+    // Read and show the information of an User
+        public function show($id)
+    {
+        $user= Users::find($id);
+        return $user;
+    }
+
+
+    public function update(Request $request, $id)
+
+    {
+             $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required',
+                                         ]);
+            Users::find($id)->update($request->all());
+    }
+
+        public function destroy($id)
+
+    {
+
+        Users::find($id)->delete();
+
+    }
+  
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//USER CONTROLLER
+Route::resource('users', 'UsersController');
