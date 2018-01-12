2ef2d00fa2aad76a03d0bcf52389b48744da42a9
Mykooll <mykooll>
2018-01-12 21:38:18 +0100

    Create user invite blade template from HTML template #21
    Changes regarding the invite blade template. I have created this by converting the index page html template into a blade template which is to be consumed by SendGrid

---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html ng-app="mvApp">
+	<head>
+		<title>Makers Valley</title>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link rel="stylesheet" href="./css/font-awesome.min.css">
+	    <link rel="stylesheet" href="./css/materialize.min.css">
+	    <link rel="stylesheet" href="./css/styles.css">
+	</head>
+	<body>
+		<main ng-view></main>
+		<script src="./js/jquery.min.js"></script>
+		<script src="./js/materialize.min.js"></script>
+		<script src="./js/angular.min.js"></script>
+		<script src="./js/angular-route.min.js"></script>
+		<script src="./js/app.js"></script>
+		<script src="./js/styles.js"></script>
+		<script src="./js/controllers/home-controller.js"></script>
+	</body>
+</html>
