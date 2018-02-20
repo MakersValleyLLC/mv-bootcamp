0db5988ef2cb6c1c01b92e54fa3a4d914fd0d784
Enoch Sowah <enoch.sowah16@gmail.com>
2018-02-20 14:47:44 -0800

    api init

--- frontend/public/js/controllers/login-controller.js
+++ frontend/public/js/controllers/login-controller.js
@@ -1,5 +1,24 @@
 //Controller for the login page
-app.controller('loginCtrl', function($scope){
+app.controller('loginCtrl', function($scope,$http,$location){
+	$scope.login = {}; //object to hold user login details
+	$scope.resMsg = ""; //scope variable to display the login response message
+	$scope.userDetails = {}; //object to hold user details after successful login
 
-	console.log('login page initialized');
+	//method to log user in
+	$scope.loginUser = function() {
+		$http.post('bootcamp.mv/api/users/login',$scope.login) //make http request to the endpoint url
+		.then(function success(response) { //if the http request was successful
+			if (response == "Error from data typed" || response == " E-mail not registered ") { //if any of the error messages is shown
+				$scope.resMsg = "Login Failed. Wrong Username Or Password";
+			} else {	//if no error messages are shown
+				$scope.userDetails = response; //set the user details object with the user's details from the db
+				$scope.resMsg = "Login Successful."; //show response message on template
+				$location.path('/home'); //relocate the user to appropriate page, home page for now.
+			}
+			
+
+		}, function error(error) { // if the http request was not successful
+			$scope.resMsg = "Could Not Log Into Your Account. Please Try Again."; // show this error message on the template
+		});
+	}
 });
--- frontend/public/js/controllers/signup-controller.js
+++ frontend/public/js/controllers/signup-controller.js
@@ -1,5 +1,17 @@
 //Controller for the signup page
-app.controller('signupCtrl', function($scope){
-	
-	console.log('signup page initialized');
+app.controller('signupCtrl', function($scope,$http,$location){
+	$scope.signup = {};
+	$scope.resMsg = "";
+
+	$scope.signupUser = function() {
+		$http.post('bootcamp.mv/api/users/create/user',$scope.signup).then(function success(response) {
+			if (response == "400 - error json") {
+				$scope.resMsg = "Could Not Sign Up";
+			} else if (response == "User Created") {
+				$scope.resMsg = "Signup Successful";
+			}
+		}, function error(error) {
+			$scope.resMsg = "Could Not Sign Up. Please Try Again.";
+		})
+	}
 });
